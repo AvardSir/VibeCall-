c14363f4c6cb9338ff49666f899334e48ca57d44
AvardSir <114823780+AvardSir@users.noreply.github.com>
2026-07-01 13:11:49 +0300

    feat(frontend): invalid-link (call-not-found) screen

--- frontend/src/features/room-states/index.ts
+++ frontend/src/features/room-states/index.ts
@@ -2,3 +2,4 @@ export { CallFullScreen } from './CallFullScreen';
 export type { CallFullScreenProps } from './CallFullScreen';
 export { ConnectErrorScreen } from './ConnectErrorScreen';
 export type { ConnectErrorScreenProps } from './ConnectErrorScreen';
+export { InvalidLinkScreen } from './InvalidLinkScreen';
